9d61e373f986885dfb4375c8418ffb303f06275c
Akshay Gattani <agattani@vt.edu>
2021-09-15 19:06:30 -0400

    Topological Sort Algorithm

--- Algorithms/TopologicalSort.cpp
+++ Algorithms/TopologicalSort.cpp
@@ -8,13 +8,18 @@ using namespace std;
 // Return an empty array otherwise
 vector<int> topologicalSort(vector<vector<int>> graph) {
   int n = graph.size();
+
+  // Stores the topological ordering
   vector<int> ordering(n, -1);
+
+  // Stores the in degree for each node
   vector<int> inDegree(n, 0);
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < graph[i].size(); j++) {
       inDegree[graph[i][j]]++;
     }
   }
+
   int index = 0;
   for (int i = 0; i < n; i++) {
     if (inDegree[i] == 0) {
@@ -24,14 +29,22 @@ vector<int> topologicalSort(vector<vector<int>> graph) {
   if (index == n) {
     return ordering;
   }
+
   for (int i = 0; i < n; i++) {
+    // If no node with in degree exist, then a topological ordering is not
+    // possible.
     if (ordering[i] == -1) {
       return {};
     }
+
     for (int j = 0; j < graph[ordering[i]].size(); j++) {
+      // Remove the edges from the node which is to be deleted
       inDegree[graph[ordering[i]][j]]--;
       if (inDegree[graph[ordering[i]][j]] == 0) {
         ordering[index++] = graph[ordering[i]][j];
+        if (index == n) {
+          return ordering;
+        }
       }
     }
   }
