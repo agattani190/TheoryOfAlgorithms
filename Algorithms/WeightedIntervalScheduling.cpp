c7cbf816fcc235fa88330e2306b9654df6d9cc1a
Akshay Gattani <88683233+agattani190@users.noreply.github.com>
2021-10-12 13:32:32 -0400

    Weighted Interval Scheduling Algorithm

--- Algorithms/WeightedIntervalScheduling.cpp
+++ Algorithms/WeightedIntervalScheduling.cpp
@@ -6,15 +6,26 @@ using namespace std;
 int weightedIntervalScheduling(vector<int> &startTime, vector<int> &endTime,
                                vector<int> &weight) {
   int n = startTime.size();
+
+  // Store all the info in a single array
   vector<pair<pair<int, int>, int>> intervals(n);
   for (int i = 0; i < n; i++) {
     intervals[i] = {{startTime[i], endTime[i]}, weight[i]};
   }
+
+  // Sort intervals based on start time
   sort(intervals.begin(), intervals.end());
+
+  // Store the start time in a separate array to easily apply binary search to
+  // find the next valid interval compatible with the one chosen
   for (int i = 0; i < n; i++) {
     startTime[i] = intervals[i].first.first;
   }
+
   int nextValidIndex;
+
+  // Store the previous results in an array
+  // dp[i] stores the result for the problem with intervals from i to n
   vector<int> dp(n, 0);
   dp[n - 1] = intervals[n - 1].second;
   for (int i = n - 2; i >= 0; i--) {
